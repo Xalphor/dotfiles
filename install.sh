a903a36488f1a28032c3a844a2a4af255b50a5b2
Holger Illhardt <holger.illhardt@contact-software.com>
2023-06-07 16:37:45 +0200

    add dependency check

--- install.sh
+++ install.sh
@@ -9,8 +9,23 @@ fi
 # One line to get the exact location of this script.
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-# INSTALLS and CONFIGURES NeoVIM
+check_dependencies(){
+    # CHECKS if all dependency packages are installed. 
+    missing_packages=""
+
+    if ! command -v fzf &> /dev/null;
+    then
+        missing_packages="fzf"
+    fi
+
+    if [ ! -z $missing_packages ]
+    then
+        echo "The following packages are required but missing, please install them first and then return this script: $missing_packages"
+    fi
+}
+
 setup_nvim() {
+    # INSTALLS and CONFIGURES NeoVIM
     if ! command -v nvim &> /dev/null;
     then
             echo "NeoVIM not found. Please check https://neovim.io/ on how to install it."
@@ -25,6 +40,8 @@ setup_nvim() {
 
 echo "- Starting Dotfile Setup v1.0 -"
 
+
+check_dependencies
 setup_nvim
 
 echo "- DONE -"
